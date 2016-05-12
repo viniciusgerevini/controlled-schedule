c28445dc0e121e12632c867a14e1cef5c6a0696f
Vinicius Gerevini <viniciusgerevini@gmail.com>
2016-05-12 23:58:06 +1000

    stopAfter improvements
    - stop after timer starts to count only after schedule start
    - stop after can be called twice to reschedule the stop time

--- test/stop-after.spec.js
+++ test/stop-after.spec.js
@@ -44,4 +44,85 @@ describe('#stopAfter()', function() {
       }, 500);
     }, 1100);
   });
+
+  it('should start to count stop timer only after start', function(done) {
+    let task = function() {
+      return Promise.resolve();
+    };
+
+    let start = Date.now();
+    let schedule =
+      execute(task)
+        .every(100)
+        .stopAfter('1s');
+
+    schedule.on('stop', function() {
+      expect(Date.now() - start).to.be.above(1200);
+      done();
+    });
+
+    setTimeout(function() {
+      schedule.start();
+    }, 200);
+  });
+
+  it('should run stopAfter timer only after startIn started', function(done) {
+    let task = function() {
+      return Promise.resolve();
+    };
+
+    let start = Date.now();
+    let schedule =
+      execute(task)
+        .every(100)
+        .stopAfter('1s')
+        .startIn(200);
+
+    schedule.on('stop', function() {
+      expect(Date.now() - start).to.be.above(1199);
+      done();
+    });
+  });
+
+  it('should not create more then one stop schedule', function(done) {
+    let stopCount = 0;
+    let task = function() {
+      return Promise.resolve();
+    };
+
+    execute(task)
+      .every(100)
+      .stopAfter('500')
+      .on('stop', function() {
+        stopCount++;
+      })
+      .start()
+      .stopAfter('600');
+
+    setTimeout(function() {
+      expect(stopCount).to.be.equal(1);
+      done();
+    }, 1200);
+  });
+
+  it('should be possible to change stop timer', function(done) {
+    let task = function() {
+      return Promise.resolve();
+    };
+
+    let start = Date.now();
+    let schedule =
+      execute(task)
+        .every(100)
+        .stopAfter('2s')
+        .start()
+        .stopAfter('1s');
+
+    schedule.on('stop', function() {
+      let stopTime = Date.now() - start;
+      expect(stopTime).to.be.above(999);
+      expect(stopTime).to.not.be.above(2000);
+      done();
+    });
+  });
 });
